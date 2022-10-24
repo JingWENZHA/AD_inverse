2c378fc9c2f7db99661983ae5da2d3ffb2f93f28
ENZE XU <enze@ENZEs-MacBook-Pro-14inch.local>
2022-10-23 22:01:01 -0400

    inverse_ytrue_personalized

---
+++ DP_population_data.py
@@ -0,0 +1,53 @@
+# import networkx as nx
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import pickle
+import os
+
+from data_prepare_const import *
+
+
+def one_time_deal_PET(data_path_list=None):
+    if not data_path_list:
+        data_path_list = ["/Users/enze/Desktop/Zhang2022/AD_inverse/Data/Amyloid_Full.xlsx", "/Users/enze/Desktop/Zhang2022/AD_inverse/Data/271tau.csv", "/Users/enze/Desktop/Zhang2022/AD_inverse/Data/FDG_Full.xlsx"]
+    data_a = pd.read_csv(data_path_list[0])
+    data_t = pd.read_csv(data_path_list[1])
+    data_n = pd.read_csv(data_path_list[2])
+    data_a = data_a[COLUMN_NAMES + TITLE_NAMES]
+    data_t = data_t[COLUMN_NAMES + TITLE_NAMES]
+    data_n = data_n[COLUMN_NAMES + TITLE_NAMES]
+
+    class_number = 5
+
+    for type_name, df in zip(["PET-A", "PET-T", "PET-N"], [data_a, data_t, data_n]):
+        save_path = "data/PET/{}_{{}}.npy".format(type_name)
+        collection = np.zeros((class_number, 160))
+        counts = np.zeros(class_number)
+        for index, row in df.iterrows():
+            label = None
+            for one_key in LABELS:
+                if row["DX"] in LABELS[one_key]:
+                    label = one_key
+                    counts[LABEL_ID[label]] += 1
+                    break
+
+            if not label:
+                # print("key not found: \"{}\"".format(row["DX"]))
+                continue
+            for i in range(160):
+                collection[LABEL_ID[label]][i] += float(row[COLUMN_NAMES[i]])
+        for one_key in LABELS:
+            if counts[LABEL_ID[one_key]] != 0:
+                avg = collection[LABEL_ID[one_key], :] / counts[LABEL_ID[one_key]]
+                # print(type_name, "avg({})".format(collection[LABEL_ID[one_key], :].shape))
+                np.save(save_path.format(one_key), avg)
+        print(type_name, "counts:", counts)
+
+
+if __name__ == "__main__":
+    # one_time_deal_PET()
+    # d = one_time_build_ptid_dictionary()
+    one_time_deal_PET()
+    pass
--- plot_pred.py
+++ plot_pred.py
@@ -26,31 +26,61 @@ from utils import *
 
 if __name__ == "__main__":
     main_path = "/Users/enze/Desktop/Zhang2022/AD_inverse"
-    file_name = "2022-10-17-15-24-53_SimpleNetworkAD_id=100_20000_0.001_2022-10-17-12-12-56.npy"
-    ypred = np.load(os.path.join(main_path,"saves",'SimpleNetworkAD_id=100_2022-10-17-12-12-56',"2022-10-17-15-24-53_SimpleNetworkAD_id=100_20000_0.001_2022-10-17-12-12-56.npy"))
+    folder_name = "SimpleNetworkAD_id=100_2022-10-23-21-41-43_sub=1"
+    file_name = "2022-10-23-21-47-22_SimpleNetworkAD_id=100_40000_0.001_2022-10-23-21-41-43.npy"
+    ypred = np.load(os.path.join(main_path,"saves",folder_name,file_name))
 
     mat = scipy.io.loadmat('./Data/data_20220915.mat')
     y_true = torch.tensor(mat['ptData_stacked_20220915'][:, :, :].reshape(184, 13, 3)).float()
     y_true[y_true < 0] = 0
     y_true_month = torch.tensor(mat['ptMonth_stacked_20220915'][:, :].reshape(184, 13)).float()
-
-    for i in range(10):
+    print(ypred[:, 0])
+    for i in range(1):
         # print(y_true_month[i]/0.1)
-        if i == 8:
-            print(y_true[i, :, :])
         plt.figure(figsize=(8,6))
 
-        plt.plot(range(1630), ypred[i,:,0],'r')
-        plt.plot(y_true_month[i]/0.1, y_true[i, :, 0], 'ro')
+        plt.plot(range(1630), ypred[:, 0], 'r')
+        plt.plot(y_true_month[i+1] / 0.1, y_true[i+1, :, 0], 'ro')
 
-        plt.plot(range(1630), ypred[i, :, 1],'g')
-        plt.plot(y_true_month[i]/0.1, y_true[i, :, 1], 'gx')
+        plt.plot(range(1630), ypred[:, 1], 'g')
+        plt.plot(y_true_month[i+1] / 0.1, y_true[i+1, :, 1], 'gx')
 
-        plt.plot(range(1630), ypred[i, :, 2],'b')
-        plt.plot(y_true_month[i]/0.1, y_true[i, :, 2], 'bd')
+        plt.plot(range(1630), ypred[:, 2], 'b')
+        plt.plot(y_true_month[i+1] / 0.1, y_true[i+1, :, 2], 'bd')
 
         plt.legend(["A_pred","A_true","T_pred","A_true","N_pred","N_true"])
 
 
-        plt.show()
-        plt.close()
+        # plt.plot(range(1630), ypred[i,:,0],'r')
+        # plt.plot(y_true_month[i]/0.1, y_true[i, :, 0], 'ro')
+        #
+        # plt.plot(range(1630), ypred[i, :, 1],'g')
+        # plt.plot(y_true_month[i]/0.1, y_true[i, :, 1], 'gx')
+        #
+        # plt.plot(range(1630), ypred[i, :, 2],'b')
+        # plt.plot(y_true_month[i]/0.1, y_true[i, :, 2], 'bd')
+
+
+
+        # m = MultiSubplotDraw(row=3, col=3, fig_size=(39, 30), tight_layout_flag=True, show_flag=True, save_flag=False)
+        # for i in range(3):
+        #
+        # # param_ls = np.asarray(param_ls)
+        # # param_true = np.asarray(param_true)
+        # # labels = ["k_a", "k_t", "k_tn", "k_an", "k_atn"]
+        # # for i in range(len(param_ls[0])):
+        # #     m.add_subplot(x_list=[j for j in range(param_ls.shape[0])], y_lists=[param_ls[:, i], param_true[:, i]],
+        # #                   color_list=['b', 'black'], fig_title=labels[i], line_style_list=["solid", "dashed"],
+        # #                   legend_list=["para_pred", "para_truth"])
+        # #
+        # # error_ka = abs(((param_ls[:, 0] - param_true[:, 0]) / param_true[:, 0]))
+        # # error_kt = abs(((param_ls[:, 1] - param_true[:, 1]) / param_true[:, 1]))
+        # # error_ktn = abs(((param_ls[:, 2] - param_true[:, 2]) / param_true[:, 2]))
+        # # error_kan = abs(((param_ls[:, 3] - param_true[:, 3]) / param_true[:, 3]))
+        # # error_katn = abs(((param_ls[:, 4] - param_true[:, 4]) / param_true[:, 4]))
+        # #
+        # # m.add_subplot(x_list=[j for j in range(param_ls.shape[0])],
+        # #               y_lists=[error_ka, error_kt, error_ktn, error_kan, error_katn], color_list=['b'] * 5,
+        # #               fig_title="relatve error", line_style_list=["solid"] * 5, legend_list=labels)
+        #
+        # m.draw()
